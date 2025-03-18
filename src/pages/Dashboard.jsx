bd498541ed89c1db746f282744fbb129c9aa93e3
unknown <nicolepromesseirakoze@gmail.com>
2025-03-18 15:37:28 -0700

    responsive dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
-
+import { Search, Bell } from "lucide-react";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../config/firebase";
+import { User,Home,BarChart,Cog, Users,Calendar, PieChart, CalendarDays  } from "lucide-react";
+
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -11,14 +13,109 @@ const Dashboard = () => {
     navigate("/login"); // Redirect to login after logout
   };
 
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
-      <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 mt-4">
-        Logout
+return(
+<div className="bg-gray-50 min-h-screen">
+  <div className="bg-white border border-gray-200 w-full h-10">
+<h1 className="text-green-800 font-bold  mb-1 ml-4">dashboard</h1>
+<div className="flex items-center  rounded-lg px-3 py-2 w-full max-w-md ml-25 -mt-8">
+        <Search className="w-5 h-5 text-gray-500" />
+        <input
+          type="text"       
+          className="ml-2 w-full outline-none bg-transparent"
+        />
+      </div>
+        <Bell className="w-5 h-5 ml-38 -mt-7" />     
+  </div>
+  <button 
+        className="flex items-center gap-2 p-2  transition"
+        onClick={() => setOpen(!open)}
+      >
+        <User className="w-6 h-6  ml-280 -mt-13" />
+        <p className="-mt-14 text-xl font-bold text-gray-600">Christine</p>
       </button>
-    </div>
-  );
+
+      <div className="bg-white border border-gray-100 w-50 h-190 -mt-4">
+      <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+            <Home className="w-5 h-5 text-blue-950" />
+            <span className="font-bold">Dashboard</span>
+          </a>
+        <div>
+        <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+            <User className="w-5 h-5 text-blue-950" />
+            <span className="font-bold">Customer</span>
+          </a>
+          <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+            <BarChart className="w-5 h-5 text-blue-950" />
+            <span className="font-bold">Analytics</span>
+          </a>
+        </div>
+        <hr className="border-t border-gray-300 my-3 mr-5" />
+        <div>
+        <h1 className="text-gray-500 font-bold ml-2">MANAGEMENT</h1>
+         
+        <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+          <Cog className="w-5 h-5 text-blue-950" />
+          <span className="font-bold">Setting</span>
+        </a>
+        <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+          <Users className="w-5 h-5 text-blue-950" />
+          <span className="font-bold">Analytics</span>
+        </a>
+        <hr className="border-t border-gray-300 my-3 mr-5" />
+        </div>
+        <div>
+        <h1 className="text-gray-500 font-bold ml-2">REPORT</h1>
+        <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+          <Calendar className="w-5 h-5 text-blue-950" />
+          <span className="font-bold">Analytics</span>
+        </a>
+
+        <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+          <PieChart className="w-5 h-5 text-blue-950" />
+          <span className="font-bold">Analytics</span>
+        </a>
+
+        <a href="#" className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-700 transition">
+          < CalendarDays className="w-5 h-5 text-blue-950" />
+          <span className="font-bold">Analytics</span>
+        </a>
+        </div>
+      </div>
+      
+    
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 -mt-180 ml-60 w-220">
+          {[
+            { title: "Total Revenue", value: "$84,254", desc: "from last month" },
+            { title: "Active Users", value: "2,420", desc: "from last week" },
+            { title: "New Customers", value: "1,320", desc: "from last week" },
+            { title: "Total Orders", value: "5,780", desc: "from last week" }
+          ].map((stat, index) => (
+            <div key={index} className="bg-white p-4 rounded-lg shadow border">
+              <h1 className="text-gray-500 text-sm font-bold">{stat.title}</h1>
+              <p className="text-2xl font-bold mt-2">{stat.value}</p>
+              <p className="text-gray-500 text-sm mt-1">{stat.desc}</p>
+            </div>
+          ))}
+        </div>
+<div className="ml-60 bg-white mt-8 w-217 h-50 border border-gray-200 rounded-lg">
+  <h1 className="font-bold ml-5 mt-3">Recent Avtivities</h1>
+  <hr className="border-t border-gray-300 my-3 w-216" />
+
+  <User className="w-5 h-5 ml-5 text-blue-950" />
+  <h1 className="font-bold ml-12 -mt-5">Mark Thomas made a purchase</h1>
+  <hr className="border-t border-gray-300 my-3 w-216" />
+
+  <User className="w-5 h-5 ml-5 text-blue-950" />
+  <h1 className="font-bold ml-12 -mt-5">Sarah willson joined the team</h1>
+  <hr className="border-t border-gray-300 my-3 w-216" />
+
+<User className="w-5 h-5 ml-5 text-blue-950" />
+  <h1 className="font-bold ml-12 -mt-5">David Brown update their profile</h1>
+ 
+</div>
+
+</div>
+);
 };
 
 export default Dashboard;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,66 +21,64 @@ const Login = () => {
   };
 
   return (
-    <div>
-    <div className="flex items-center justify-center h-screen bg-gray-50 ">
-      <form
-        onSubmit={handleLogin}
-        className="bg-gray-50 p-8 shadow-lg rounded-lg w-96 flex flex-col items-center ml-160 "
-      >
+    <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen bg-gray-50 p-6">
+      {/* Image Section */}
+      <div className="hidden lg:flex flex-col mr-30 items-center text-center text-white bg-green-900 p-8 rounded-lg shadow-lg ml-8 max-w-sm">
+        <h1 className="text-2xl font-bold">Welcome Back!</h1>
+        <p className="text-sm mt-4">Access your workspace and continue your journey with us.</p>
+      </div>
+
+      {/* Form Section */}
+      <div className="bg-gray-50 p-8 shadow-lg rounded-lg w-full max-w-md flex flex-col items-center">
         <h2 className="text-2xl font-bold mb-2">Sign in to your account</h2>
         <p className="text-sm text-gray-500 mb-6">
           Enter your credentials to access your account
         </p>
 
-        {/* Email Input */}
-        <div className="w-full">
-          <label className="block text-gray-700 mb-1">Email address</label>
-          <input
-            type="email"
-            placeholder="Enter your Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border border-gray-300 rounded-xl p-2 w-full bg-white"
-          />
-        </div>
+        <form onSubmit={handleLogin} className="w-full">
+          {/* Email Input */}
+          <div className="w-full mb-4">
+            <label className="block text-gray-700 mb-1">Email address</label>
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-300 rounded-xl p-2 w-full bg-white"
+            />
+          </div>
 
-        {/* Password Input */}
-        <div className="w-full mt-4">
-          <label className="block text-gray-700 mb-1">Password</label>
-          <input
-            type="password"
-            placeholder="Enter your Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border border-gray-300 rounded-xl p-2 w-full bg-white"
-          />
-        </div>
+          {/* Password Input */}
+          <div className="w-full mb-4">
+            <label className="block text-gray-700 mb-1">Password</label>
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 rounded-xl p-2 w-full bg-white"
+            />
+          </div>
 
-        {/* Forgot Password Link */}
-        <div className="w-full text-right mt-2">
-          <Link to="/forgot-password" className="text-green-900 hover:underline text-sm">
-            Forgot password?
-          </Link>
-        </div>
+          {/* Forgot Password Link */}
+          <div className="w-full text-right mb-4">
+            <Link to="/forgot-password" className="text-green-900 hover:underline text-sm">
+              Forgot password?
+            </Link>
+          </div>
 
-        {/* Submit Button */}
-        <button
-          type="submit"
-          className="bg-green-900 text-white px-4 py-2 rounded-xl mt-5 w-full hover:bg-green-800 transition"
-        >
-          Sign in
-        </button>
-      </form>
-    
-    </div>
-    <div className="flex justify-center items-center h-screen -mt-180">
-      <img src={image} alt="office" className="w-180 h-145 shadow-lg mt-60 mr-140 " />
-     
-    </div>
-    <div>
-    <h1 className="ml-15 text-2xl text-white font-bold">Welcome Back!</h1>
-    <p className="ml-15 text-sm text-white mt-5">Access you workspace and continue your journey with us.</p>
-    </div>
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="bg-green-900 text-white px-4 py-2 rounded-xl w-full hover:bg-green-800 transition"
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+
+      {/* Welcome Message Section */}
+      
     </div>
   );
 };
